88427e6a60c38f0da60cf0b05cbe2214eef2bdef
José A. Q. C. Gomes <jose.augusto.comparotto@gmail.com>
2024-10-18 12:10:20 -0400

    Arquivos movidos

---
+++ Aulas/Aula06/Exemplos/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "ArvoreBinaria.h"
+
+int main(){
+	
+    int N = 8, dados[8] = {50,100,30,20,40,45,35,37};
+
+    ArvBin* raiz = cria_ArvBin();
+
+    for(int i=0; i < N; i++)
+        insere_ArvBin(raiz,dados[i]);
+        
+    printf("Pesquisa em Ordem\n");
+	emOrdem_ArvBin(raiz);
+	
+	//printf("\nPesquisa em Pré Ordem\n");
+	//preOrdem_ArvBin(raiz);
+	
+	//printf("\nPesquisa em Pós Ordem\n");
+	//posOrdem_ArvBin(raiz);
+	
+	printf("\nAltura da árvore = %d\n", altura_ArvBin(raiz));
+
+    libera_ArvBin(raiz);
+
+    return 0;
+}
---
+++ Aulas/Aula07/Exemplos/main.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArvoreAVL.h"
+
+int main(){
+    ArvAVL* avl;
+    int res,i;
+    //int N = 10, dados[10] = {50,25,10,5,7,3,30,20,8,15};
+    //int N = 11, dados[11] = {50,25,10,5,5,7,3,30,20,8,15};
+    int N = 10, dados[10] = {1,2,3,10,4,5,9,7,8,6};
+
+    avl = cria_ArvAVL();
+
+    for(i=0;i<N;i++){
+        printf("========================\n");
+        printf("Inserindo: %d\n",dados[i]);
+        res = insere_ArvAVL(avl,dados[i]);
+    }
+
+    printf("\nAVL tree:\n");
+    emOrdem_ArvAVL(avl);
+    printf("\n\n");
+
+	//Remoções
+    remove_ArvAVL(avl,6);
+    printf("\nAVL tree:\n");
+    emOrdem_ArvAVL(avl);
+    printf("\n\n");
+
+    remove_ArvAVL(avl,7);
+    printf("\nAVL tree:\n");
+    emOrdem_ArvAVL(avl);
+    printf("\n\n");
+
+    remove_ArvAVL(avl,4);
+    printf("\nAVL tree:\n");
+    emOrdem_ArvAVL(avl);
+    printf("\n\n");
+
+    libera_ArvAVL(avl);
+
+    return 0;
+}
